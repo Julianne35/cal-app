aece28388d9d183edaa862a3775ffadc8e50676c
Julianne35 <jbosko28@gmail.com>
2022-12-01 16:01:34 -0500

    test to fix herku refresh

--- server/server.js
+++ server/server.js
@@ -18,10 +18,10 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from server YEY ME!" });
 });
 
-//All other GET requests not handled before will return our React app
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-// });
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
