7146b20460c46467ea7b5d9f32f0ddeaedaf14b2
Julianne35 <jbosko28@gmail.com>
2023-02-04 22:48:53 -0500

    removed useEfffect

--- client/src/Components/Create/CreateEvent.js
+++ client/src/Components/Create/CreateEvent.js
@@ -1,13 +1,27 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import TimePicker from "react-time-picker";
 import DatePicker from "react-date-picker";
 import Wrapper from "../UI/Wrapper";
-import style from "../UI/Wrapper";
+import style from "./Create.module.css";
 import globalStyle from "../UI/style.css";
 import UserButton from "../UI/UserButton";
-import { Form, Button, InputGroup} from "react-bootstrap";
+import { Form, Button, InputGroup } from "react-bootstrap";
 
 const CreateEvent = () => {
+  const [contactname, setContactName] = useState("");
+  const [contactphone, setContactPhone] = useState("");
+  const [contactemail, setContactEmail] = useState("");
+  const [event, setEvent] = useState("");
+  const [details, setDetails] = useState([]);
+  const [eventaddress, setEventAddress] = useState([]);
+  const [street, setStreet] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("NY");
+  const [zipcode, setZipCode] = useState("");
+
+  const subLink = `/create`;
+
   const [timeValue, setTimeValue] = useState(new Date());
   const [dateValue, setDateValue] = useState(new Date());
   const header = style.header;
@@ -17,26 +31,83 @@ const CreateEvent = () => {
   const nyInput = style.nyInput;
   const cityInput = style.cityInput;
   const upload = style.uploadBtn;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //function needed to post address prop (first step)
+    setEventAddress((eventaddress) => [
+      ...eventaddress,
+      { street: street, city: city, state: state, zipcode: zipcode },
+    ]);
+    setDetails((details) => [
+      ...details,
+      {
+        contactname: contactname,
+        contactphone: contactphone,
+        contactemail: contactemail,
+        //address data posted from nested obj array within obj array (2nd step)
+        eventaddress: [{ ...eventaddress, street, city, state, zipcode }],
+      },
+      // ]);
+    ]);
+  };
+
+  axios({
+    method: "POST",
+    url: "http://localhost:3000/post",
+    data: {
+      details: details,
+      event: event,
+    },
+  }).then((res) => {
+    console.log("resp", res.data);
+  });
+
   return (
     <>
       <Wrapper>
-      <h1 className={header}>Create Event</h1>
+        <h1 className={header}>Create Event</h1>
         <h2 className={subHeader}>Contact Info</h2>
         <hr />
-      <Form>
-          <Form.Group className={padding} controlId="formBasicContactName">
-            <Form.Control type="text" placeholder="Contact Name" />
+        <Form action="/post" method="post">
+          <Form.Group className={padding} controlId="contactname">
+            <Form.Control
+              type="text"
+              name="contactname"
+              value={contactname}
+              onChange={(e) => setContactName(e.target.value)}
+              placeholder="Contact Name"
+            />
           </Form.Group>
-          <Form.Group className={padding} controlId="formBasicContactPhone">
-            <Form.Control type="text" placeholder="Contact Phone" />
+          <Form.Group className={padding} controlId="contactphone">
+            <Form.Control
+              type="number"
+              name="contactphone"
+              value={contactphone}
+              onChange={(e) => setContactPhone(e.target.value)}
+              placeholder="Contact Phone"
+            />
           </Form.Group>
-          <Form.Group className={padding} controlId="formBasicContactEmail">
-            <Form.Control type="email" placeholder="Contact Email" />
+          <Form.Group className={padding} controlId="contactemail">
+            <Form.Control
+              type="email"
+              name="contactemail"
+              value={contactemail}
+              onChange={(e) => setContactEmail(e.target.value)}
+              placeholder="Contact Email"
+            />
           </Form.Group>
           <h2 className={subHeader}>Event Info</h2>
           <hr />
-          <Form.Group className={padding} controlId="formBasicEventName">
-            <Form.Control type="text" placeholder="Event Name" />
+          <Form.Group className={padding} controlId="eventname">
+            <Form.Control
+              type="text"
+              name="eventname"
+              value={event}
+              onChange={(e) => setEvent(e.target.value)}
+              placeholder="Event Name"
+            />
           </Form.Group>
 
           <div className={flexWrap}>
@@ -67,22 +138,46 @@ const CreateEvent = () => {
           </div>
 
           <div className={flexWrap}>
-            <Form.Group controlId="formBasicStreet">
-              <Form.Control type="text" placeholder="Street" />
+            <Form.Group controlId="street">
+              <Form.Control
+                type="text"
+                name="street"
+                value={street}
+                onChange={(e) => setStreet(e.target.value)}
+                placeholder="Street"
+              />
             </Form.Group>
-            <Form.Group className={cityInput} controlId="formBasicCity">
-              <Form.Control type="text" placeholder="City" />
+            <Form.Group className={cityInput} controlId="city">
+              <Form.Control
+                type="text"
+                name="city"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                placeholder="City"
+              />
             </Form.Group>
-            <Form.Select className={nyInput}>
-              <option>NY</option>
-              <option>NJ</option>
-              <option>PA</option>
+            <Form.Select
+              className={nyInput}
+              type="text"
+              name="state"
+              value={state}
+              onChange={(e) => setState(e.target.value)}
+            >
+              <option value="NY">NY</option>
+              <option value="NJ">NJ</option>
+              <option value="PA">PA</option>
             </Form.Select>
           </div>
 
           <div className={padding}>
-            <Form.Group controlId="formBasicZipCode">
-              <Form.Control type="number" placeholder="Zip Code" />
+            <Form.Group controlId="zipcode">
+              <Form.Control
+                type="number"
+                name="zipcode"
+                value={zipcode}
+                onChange={(e) => setZipCode(e.target.value)}
+                placeholder="Zip Code"
+              />
             </Form.Group>
 
             <Button className={upload} variant="secondary">
@@ -94,8 +189,12 @@ const CreateEvent = () => {
             <Form.Control as="textarea" placeholder="Event Description" />
           </InputGroup>
 
-          <UserButton linkTo={`/success`} className={style.btn}>
-            SUBMIT FOR REVIEW
+          <UserButton
+            linkTo={subLink}
+            className={style.btn}
+            onClick={handleSubmit}
+          >
+            CREATE AND REVIEW
           </UserButton>
         </Form>
       </Wrapper>
@@ -103,3 +202,5 @@ const CreateEvent = () => {
   );
 };
 export default CreateEvent;
+
+//will not post unless all data fields are being called, you cant just test out one field at a time unless you comment data on model on server
